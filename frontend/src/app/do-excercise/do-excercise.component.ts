ef88ed7cef6dccafb79e4b56311ed73b23eb393a
Ivan Zyranau <ewancoder@gmail.com>
2024-10-18 16:46:47 +0400

    Add rest timer for doing a single set

--- frontend/src/app/do-excercise/do-excercise.component.ts
+++ frontend/src/app/do-excercise/do-excercise.component.ts
@@ -1,14 +1,15 @@
-import { DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, OnInit, signal, WritableSignal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Excercise, Set } from '../models';
 import { WorkoutService } from '../workout.service';
 
 @Component({
     selector: 'olab-do-excercise',
     standalone: true,
-    imports: [DatePipe, ReactiveFormsModule],
+    imports: [AsyncPipe, DatePipe, ReactiveFormsModule],
     templateUrl: './do-excercise.component.html',
     styleUrl: './do-excercise.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -16,6 +17,7 @@ import { WorkoutService } from '../workout.service';
 export class DoExcerciseComponent implements OnInit {
     @Input({ required: true }) workoutExcerciseIndex!: string;
     @Input({ required: true }) excerciseId!: string;
+    restTimerSignal = signal<Observable<string> | undefined>(undefined);
 
     public excercise: WritableSignal<Excercise | undefined> = signal(undefined);
     public currentSetsSignal: WritableSignal<Set[]> = signal([]);
@@ -43,6 +45,7 @@ export class DoExcerciseComponent implements OnInit {
             .subscribe(currentSets => {
                 this.currentSetsSignal.set(currentSets);
                 this.addSetForm.controls.reps.reset();
+                this.restTimerSignal.set(this.service.createStopwatch(currentSets.at(-1)!.date));
             });
     }
 
