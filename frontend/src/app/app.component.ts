85542ce5407b947d03301f7fb0ab442846c01e7c
Ivan Zyranau <ewancoder@gmail.com>
2025-08-06 08:50:13 +0400

    Save application settings between sessions

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './theme.service';
-import { Set } from './set/set.component';
-import { RepType } from './rep/rep.component';
 import { SettingsService } from './settings.service';
-import { PerformanceComponent } from './performance/performance.component';
 
 @Component({
     selector: 'olab-root',
-    imports: [RouterOutlet, PerformanceComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush
--- frontend/src/app/settings.service.ts
+++ frontend/src/app/settings.service.ts
@@ -1,20 +1,35 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, WritableSignal } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class SettingsService {
-    useLbs = signal(false);
-    showRepTypes = signal(false);
-    compactView = signal(false);
+    useLbs: WritableSignal<boolean>;
+    showRepTypes: WritableSignal<boolean>;
+    compactView: WritableSignal<boolean>;
+
+    constructor() {
+        this.useLbs = signal(localStorage.getItem('tyr_olab_useLbs') === 'true');
+        this.showRepTypes = signal(localStorage.getItem('tyr_olab_showRepTypes') === 'true');
+        this.compactView = signal(localStorage.getItem('tyr_olab_compactView') === 'true');
+    }
 
     toggleUnits() {
-        this.useLbs.update(units => !units);
+        this.useLbs.update(units => {
+            localStorage.setItem('tyr_olab_useLbs', JSON.stringify(!units));
+            return !units;
+        });
     }
 
     toggleRepTypes() {
-        this.showRepTypes.update(show => !show);
+        this.showRepTypes.update(show => {
+            localStorage.setItem('tyr_olab_showRepTypes', JSON.stringify(!show));
+            return !show;
+        });
     }
 
     toggleCompactView() {
-        this.compactView.update(compact => !compact);
+        this.compactView.update(compact => {
+            localStorage.setItem('tyr_olab_compactView', JSON.stringify(!compact));
+            return !compact;
+        });
     }
 }
